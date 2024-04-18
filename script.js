10b7e1eb0a715db46953ddc0174d6133605859ec
nalve <nestor.alvz@gmail.com>
2024-04-18 15:42:04 -0600

    Sorpresa de cumpleaños para Oscar

---
+++ script.js
@@ -0,0 +1,6 @@
+// Agrega cualquier interactividad o efectos especiales aquí
+
+// Ejemplo: Mostrar un mensaje de alerta cuando se carga la página
+window.onload = function() {
+    alert("¡Sorpresa! ¡Feliz cumpleaños, Oscar!");
+};
